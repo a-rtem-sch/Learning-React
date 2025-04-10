f5ca260e9756cada69645e8931d439ae2e0c803e
Artem <artemsch1711@gmail.com>
2025-04-10 18:00:02 +0300

    Initial commit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+export  const Counter = function() {
+    const [count, setCount] = useState(0)
+
+    function increment() {
+        setCount(count + 1)
+        // console.log(count)
+    }
+    function decrement() {
+        setCount(count - 1)
+        // console.log(count)
+    }
+
+    return(
+
+        <div>
+            <h1>{count}</h1>
+            
+            <button onClick={increment}>Press me!</button>
+            <button onClick={decrement}>Press me to decrease!</button>
+        </div>
+    )
+}
+
+export default Counter
